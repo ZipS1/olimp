b3fe4bf558e960c7120d90f623d08f08825b8cb6
ZipS1 <czwetkow.serega@yandex.ru>
2020-11-10 20:19:45 +0300

    TODO - in brackets.py

--- brackets.py
+++ brackets.py
@@ -1,23 +1,20 @@
-s = input("Enter string: ")
+s = input("Enter string: ")                    #TODO: refactorig breaks (lines 17, 20) with in another way
 symbols = ('(', ')', '[', ']', '{', '}')
 
 for i in s:
-	if i not in symbols:
-		s.remove(i)
-print("[DEBUG]", s)
-# while True:
-# 	if s.find("()") != -1:
-# 		s = s.replace('()', '')
-# 	elif s.find("[]") != -1:
-# 		s = s.replace('[]', '')
-# 	elif s.find(r"{}") != -1:
-# 		s = s.replace(r'{}', '')
-# 	else:
-# 		if s == '':
-# 			print("Скобки расставлены верно")
-# 			break
-# 		else:
-			# print("Скобки расставлены неверно")
-			# break
-
-
+    if i not in symbols:
+         s = s.replace(i, '', 1)
+while True:
+    if s.find("()") != -1:
+        s = s.replace('()', '')
+    elif s.find("[]") != -1:
+        s = s.replace('[]', '')
+    elif s.find(r"{}") != -1:
+        s = s.replace(r'{}', '')
+    else:
+        if s == '':
+            print("correct")
+            break
+        else:
+            print("incorrect")
+            break
