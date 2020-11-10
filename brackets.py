ce66f1f3d075aef582afa4c19795ddd07d8a5941
ZipS <czwetkow.serega@yandex.ru>
2020-11-10 11:10:15 +0300

    TODO: complete brackets.py

--- brackets.py
+++ brackets.py
@@ -1,26 +1,23 @@
 s = input("Enter string: ")
-symbols = ('(', ')', '[', ']', '{', '}', '<', '>')
+symbols = ('(', ')', '[', ']', '{', '}')
 
 for i in s:
 	if i not in symbols:
-		print("Строка введена неверно!")
-		exit()
-
-while True:
-	if s.find("()") != -1:
-		s = s.replace('()', '')
-	elif s.find("[]") != -1:
-		s = s.replace('[]', '')
-	elif s.find(r"{}") != -1:
-		s = s.replace(r'{}', '')
-	elif s.find("<>") != -1:
-		s = s.replace('<>', '')
-	else:
-		if s == '':
-			print("Скобки расставлены верно")
-			break
-		else:
-			print("Скобки расставлены неверно")
-			break
+		s.remove(i)
+print("[DEBUG]", s)
+# while True:
+# 	if s.find("()") != -1:
+# 		s = s.replace('()', '')
+# 	elif s.find("[]") != -1:
+# 		s = s.replace('[]', '')
+# 	elif s.find(r"{}") != -1:
+# 		s = s.replace(r'{}', '')
+# 	else:
+# 		if s == '':
+# 			print("Скобки расставлены верно")
+# 			break
+# 		else:
+			# print("Скобки расставлены неверно")
+			# break
 
 
